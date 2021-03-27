1454033daad0d5fe3566c7e1999541b0f82ca280
sethvincent <sethvincent@gmail.com>
2021-03-27 13:13:38 -0700

    .

---
+++ download.js
@@ -0,0 +1,56 @@
+// https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=vaccination_county_condensed_data
+
+import * as fs from 'fs/promises'
+import * as path from 'path'
+
+import { format } from 'date-fns'
+import { join } from 'desm'
+import got from 'got'
+
+import { exists, writeJson } from './lib/fs.js'
+
+const url = 'https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=vaccination_county_condensed_data'
+
+async function main () {
+  try {
+    const date = format(Date.now(), 'yyyy-MM-dd')
+    const dataFilepath = join(import.meta.url, 'data', date)
+    const sourceDataFilepath = path.join(dataFilepath, 'source', 'vaccines.json')
+    const processedDataFilepath = path.join(dataFilepath, 'processed')
+    const stateDataFilepath = path.join(processedDataFilepath, 'states')
+    const states = {}
+
+    const result = await got(url, { responseType: 'json' })
+    const { body } = result
+
+    if (await exists(dataFilepath)) {
+      console.log('already got data for this date')
+      return
+    }
+
+    await fs.mkdir(path.dirname(sourceDataFilepath), { recursive: true })
+    await fs.mkdir(stateDataFilepath, { recursive: true })
+    await writeJson(sourceDataFilepath, body)
+
+    const { vaccination_county_condensed_data } = body
+
+    for (const item of vaccination_county_condensed_data) {
+      if (!states[item.StateName]) {
+        states[item.StateName] = {}
+      }
+
+      states[item.StateName][item.County] = item
+    }
+
+    for (const stateKey in states) {
+      const state = states[stateKey]
+      const stateFilepath = path.join(stateDataFilepath, `${stateKey}.json`)
+      await writeJson(stateFilepath, state)
+    }
+
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+main()
